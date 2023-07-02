67c28ddce84a51a35e20f1aedb9024c9716c67f6
Nikolay Markeev <zz5dy6@gmail.com>
2023-07-02 12:45:45 +0300

    Убран ненужный импорт и устаревший комментарий

--- src/main/java/com/example/sportmaster/service/PostQuizServiceImpl.java
+++ src/main/java/com/example/sportmaster/service/PostQuizServiceImpl.java
@@ -1,10 +1,7 @@
 package com.example.sportmaster.service;
 
-import com.example.sportmaster.openapi.model.QuizDTO;
-
 public class PostQuizServiceImpl implements IPostQuizService {
     public QuizDoc quizzesPost(QuizDoc quiz) {
-        // добавление QuizDTO, получение QuizDoc
         // изменение полей из QuizDTO
         quiz.setTitle(quiz.getTitle().toUpperCase());
         quiz.setDescription(quiz.getDescription().toUpperCase());
