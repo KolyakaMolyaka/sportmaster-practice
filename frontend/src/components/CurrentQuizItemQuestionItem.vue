d7c38aeb9deb5747fa849e329900272bfb9288d9
Nikolay Markeev <zz5dy6@gmail.com>
2023-07-07 22:42:11 +0300

    Ответы вынесены в отдельный компонент

--- frontend/src/components/CurrentQuizItemQuestionItem.vue
+++ frontend/src/components/CurrentQuizItemQuestionItem.vue
@@ -4,24 +4,23 @@
     </h2>
     <div class="card fs-5 mt-3 w-50 mx-auto"> {{ question.text }} </div>
         <div class="fs-3 mt-3 text-center fw-medium w-50 mx-auto">
-            <div class="d-flex fw-bold"> Варианты ответа </div>
+            <div class="d-flex fw-bold">Варианты ответа </div>
         </div>
         <div class="question-answers mt-4">
-            <button v-for="answer in question.answers"
-                class="answer-card btn btn-primary d-block w-50 text-start p-3 ps-5 fs-5 mt-3 rounded-pill mx-auto">
-                {{ answer.text }}, isCorrect?: {{ answer.isCorrect }}
-            </button>
+            <QuestionItemAnswer v-for="answer in question.answers" :answer="answer"/>
     </div>
 </template>
 
 <script>
+import QuestionItemAnswer from './QuestionItemAnswer.vue';
 export default {
+    components: { QuestionItemAnswer },
     props: {
         question: {
             type: Object,
             required: true
         }
-    }
+    },
 
 }
 </script>
---
+++ frontend/src/components/QuestionItemAnswer.vue
@@ -0,0 +1,37 @@
+<template>
+    <button
+    class="btn btn-primary d-block w-50 text-start p-3 ps-5 fs-5 mt-3 rounded-pill mx-auto"
+    @click="handleAnswer"
+    >
+        {{ answer.text }}
+    </button>
+
+</template>
+
+<script>
+export default {
+    props: {
+        answer: {
+            type: Object,
+            required: true
+        }
+    }, 
+    methods: {
+        handleAnswer() {
+            if (answer.isCorrect) {
+                // console.log('wrongAnswer');
+                // this.$emit('wrongAnswer', this.answer);
+            } else {
+                console.log('rightAnswer');
+                // this.$emit('rightAnswer', this.answer);
+            }
+        }
+    }
+
+}
+
+</script>
+
+
+<style>
+</style>
