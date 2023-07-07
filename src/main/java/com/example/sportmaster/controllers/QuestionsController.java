7b1320077ea8fc7872e5b1e8b657a1fa0a466a04
Nikolay Markeev <zz5dy6@gmail.com>
2023-07-07 13:32:55 +0300

    Добавлен @CrossOrigin в контроллеры

--- src/main/java/com/example/sportmaster/controllers/QuestionsController.java
+++ src/main/java/com/example/sportmaster/controllers/QuestionsController.java
@@ -12,6 +12,7 @@ import com.example.sportmaster.services.interfaces.IQuestionsService;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class QuestionsController implements QuestionsApi {
 
     @Autowired
