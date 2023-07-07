722b8b3c140425c18f0ab2b561105c59d1c4ad50
Nikolay Markeev <zz5dy6@gmail.com>
2023-07-07 13:36:58 +0300

    Добавлена возможность динамической загрузки викторин по API

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,9 @@
 <script>
 import TopLogo from './components/TopLogo.vue';
 import QuizzesList from './components/QuizzesList.vue';
+
+const testAPIUrl = 'https://jsonplaceholder.typicode.com/todos?_limit=10'
+const getQuizzesAPIUrl = 'http://127.0.0.1:8080/quizzes'
 export default {
   components: { TopLogo, QuizzesList },
   data() {
@@ -11,14 +14,23 @@ export default {
         {id: 3, title: 'Quiz #3', description: 'Quiz #3 description'},
         {id: 4, title: 'Quiz #4', description: 'Quiz #4 description'},
         {id: 5, title: 'Quiz #5', description: 'Quiz #5 description'},
-      ]
+      ],
     }
+  },
+  methods: {
+    fetchQuizzes() {
+      fetch(getQuizzesAPIUrl)
+      .then(response => response.json())
+      .then(json => this.quizzes = json);
+    },
+    
   }
 }
 
 </script>
 
 <template>
+  <button @click="fetchQuizzes">Загрузить викторины</button>
   <div class="col-md-10 mx-auto">
     <TopLogo/>
     <QuizzesList :quizzes="quizzes"/>
