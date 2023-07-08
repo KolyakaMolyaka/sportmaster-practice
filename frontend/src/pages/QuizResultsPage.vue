c9c676bdf901757aa26584fc38065c69a1a1a61d
Nikolay Markeev <zz5dy6@gmail.com>
2023-07-08 15:36:29 +0300

    Добавлено отображение текста в зависимости от набранных баллов

--- frontend/src/pages/QuizResultsPage.vue
+++ frontend/src/pages/QuizResultsPage.vue
@@ -1,9 +1,15 @@
 <template>
     <div class="h-100 d-flex justify-content-evenly align-items-center mt-5">
         <div class="quiz-card mt-3 alert alert-color">
-            <h1 class="mt-3 fw-bold text-white">Правильных ответов:  {{ $store.getters.scores }} / {{ $store.getters.fullScores }}</h1>
+            <h1 class="mt-3 fw-bold text-white">Правильных ответов:  
+                {{ $store.getters.scores }} / {{ $store.getters.fullScores }}
+            </h1>
             <div class="card fs-5 mt-3 mb-3 mx-auto">
-                Вам есть куда стремиться. Попробуйте играть чаще и тогда вы улучшите свой результат!
+                <span v-if="$store.getters.scores == $store.getters.fullScores">{{ advices['fullScore'] }}</span>
+                <span v-else-if="$store.getters.scores / $store.getters.fullScores < 0.5">{{ advices['ltHalf'] }}</span>
+                <span v-else="$store.getters.scores / $store.getters.fullScores > 0.5">{{ advices['gtHalf'] }}</span>
+                
+                
             </div>
         </div>
     </div>
@@ -11,6 +17,16 @@
 
 <script>
 export default {
+    data() {
+        return {
+            advices: {
+                'ltHalf': 'Вам есть куда стремиться. Попробуйте играть чаще и тогда вы улучшите свой результат!',
+                'gtHalf': 'Вы набрали половину правильных ответов, так держать!',
+                'fullScore': 'Вы прекрасно разбираетесь в теме.'
+
+            }
+        }
+    }
 
 }
 </script>
