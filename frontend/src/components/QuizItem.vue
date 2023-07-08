04efbf15018296528f60178f5880d81c574d69a5
Nikolay Markeev <zz5dy6@gmail.com>
2023-07-08 11:14:02 +0300

    Добавлена стилизация категории, уровня сложности, даты создания

--- frontend/src/components/QuizItem.vue
+++ frontend/src/components/QuizItem.vue
@@ -1,10 +1,22 @@
 <template>
     <h1 class="text-center mt-5 fw-bold text-white"> {{ quiz.title }} </h1>
-    <div class="card fs-5 mt-3 mx-auto"> {{ quiz.description }} </div>
-    <div class="d-flex justify-end justify-content-end mt-4">
+    <div class="card fs-5 mt-3 mx-auto ps-4 pe-4" style="text-align: justify;"> 
+        {{ quiz.description }} 
+        <span class="d-inline-flex justify-content-end fs-6 mt-2 "><em>Дата создания: {{ quiz.created_at }}</em></span>
+    </div>
+
+    <div class="d-flex mt-4">
+        <div class="d-flex align-items-start flex-column">
+            <div class="btn btn-info text-white fw-bold">
+                Категория: {{quiz.category}}
+            </div>
+            <div v-if="quiz.difficulty == 'Легкий'" class="btn btn-success mt-2 fw-bold text-white">Уровень сложности: {{  quiz.difficulty }}</div>
+            <div v-else-if="quiz.difficulty == 'Средний'" class="btn btn-warning mt-2 fw-bold">Уровень сложности: {{  quiz.difficulty }}</div>
+            <div v-else class="btn btn-danger mt-2 fw-bold text-white">Уровень сложности: {{  quiz.difficulty }}</div>
+        </div>
         <button 
         style="background-color: #cb585d !important;"
-        @click="$router.push(`/quiz/${quiz.id}`)" class="btn btn-primary fw-bold ps-4 pe-4 pt-3 pb-3">Пройти</button>
+        @click="$router.push(`/quiz/${quiz.id}`)" class="btn btn-primary fw-bold ps-4 pe-4 pt-3 pb-3 ms-auto">Пройти</button>
     </div>
 </template>
 
